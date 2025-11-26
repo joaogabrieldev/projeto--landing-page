87df4f67c160e20bd3c52d8d39c8a628fd04196c
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-11-26 18:31:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+function App() {
+  return (
+    <div className="bg-slate-950 min-h-screen flex flex-col items-center justify-center relative overflow-hidden">
+      <div className="absolute top-[-10%] left-1/2 -translate-x-1/2 w-[65vw] h-[500px] bg-fuchsia-600 opacity-35 blur-[120px] pointer-events-none z-0"></div>
+      <div className="z-1 text-white -mt-95">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+        reiciendis voluptate cum optio facilis laudantium! Tenetur, dicta
+        repellat minima ad voluptate necessitatibus? Autem enim atque
+        perspiciatis ullam dicta, ad fuga. Suscipit, ullam aliquam officia
+        aspernatur nesciunt ducimus repellendus dolorum dignissimos? Quasi
+        incidunt saepe vitae sit quos? Perferendis, quaerat ab itaque quasi
+        odit, consequuntur porro repellendus eveniet, non facilis voluptates
+        aliquam! Maiores ratione molestias aliquam molestiae culpa ullam
+        sapiente ex quis voluptatum debitis quam et provident temporibus rem,
+        quas, illo laudantium a, ducimus reiciendis nulla fugiat necessitatibus
+        beatae fugit? Perspiciatis, est? Cumque beatae, corporis facere et
+        consequatur architecto minima minus ex tenetur est, dolores commodi
+        quibusdam ipsa veritatis ut quia a alias nulla officia maxime tempora
+        nam eum? Consequatur, itaque in. Et consequatur dignissimos dicta amet
+        earum nulla qui error laudantium magnam cumque dolor voluptatem beatae
+        est repellendus aspernatur vel, unde rem, quis deleniti recusandae ipsa
+        nostrum sequi. Magnam, aut temporibus. Perspiciatis doloribus quis fugit
+        hic tempora vel fuga. Quia optio tempora saepe eius, quos cum accusamus
+        odit quis iste suscipit amet eveniet consequatur neque, labore
+        voluptates ullam, maiores ducimus delectus. Officiis accusantium
+        deleniti, maiores quae voluptate quisquam. Unde recusandae laboriosam
+        totam vero magni illo, veritatis numquam in velit, autem cum aliquam et
+        qui sunt maiores illum perspiciatis ad! Aliquid, tempore. Quae dolorum
+        cumque quod repellendus laudantium labore eaque quasi eveniet, aut
+        nostrum aliquam. Atque voluptatem tempore debitis quisquam repellat
+        nostrum quaerat sunt necessitatibus. Libero dolor quasi minus sunt ipsum
+        doloribus.
+      </div>
+    </div>
+  );
+}
+
+export default App;
