4085b1e12786d6994146ba6a07c9ea63e3a2497a
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-11-28 03:17:06 -0300

    feat(App): created semantic structure to better SEO for site

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import "./App.css"
+import "./App.css";
+import Logo from "./pieces/Logo/Logo";
 import BodyPinkGlow from "./widgets/BodyPinkGlow/BodyPinkGlow";
+import Hero from "./widgets/Hero/Hero";
 import Navbar from "./widgets/Navbar/Navbar";
+import NormalBody from "./widgets/NormalBody/NormalBody";
 
 function App() {
   return (
     <BodyPinkGlow>
-      <header className="border-2 w-full z-1">
-        <Navbar />
-      </header>
-      <main></main>
-      <footer></footer>
+      <NormalBody>
+        <header className="sticky top-0 z-1 flex w-full min-w-dvw flex-row justify-around gap-18 border-b border-b-slate-800 bg-slate-950 py-6">
+          <Logo />
+          <nav>
+            <Navbar />
+          </nav>
+        </header>
+        <main>
+          <Hero />
+        </main>
+        <footer></footer>
+      </NormalBody>
     </BodyPinkGlow>
   );
 }
