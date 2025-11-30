686f04ded5abd17db7e34844c78fbb06b932f2f3
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-11-30 12:01:07 -0300

    feat(testimonials): created testimonials section

---
+++ src/widgets/Testimonials/Testimonials.tsx
@@ -0,0 +1,11 @@
+import { motion } from "motion/react";
+
+const Testimonials = () => {
+  return (
+    <section className="relative z-1 overflow-hidden border-2 border-white">
+      <div>Testimonials</div>
+    </section>
+  );
+};
+
+export default Testimonials;
