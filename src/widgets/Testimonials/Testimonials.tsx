a69f1d31aab86d369d99671322160d5f698ace4e
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-02 12:57:38 -0300

    feat(widgets): created testimonial website section

--- src/widgets/Testimonials/Testimonials.tsx
+++ src/widgets/Testimonials/Testimonials.tsx
@@ -1,10 +1,35 @@
 import { motion } from "motion/react";
+import ModSection from "../ModSection/ModSection";
+import TestimonialCard from "../../components/TestimonialCard/TestimonialCard";
+import { testimonials } from "../../assets/testimonials";
 
 const Testimonials = () => {
   return (
-    <section className="relative z-1 overflow-hidden border-2 border-white">
-      <div>Testimonials</div>
-    </section>
+    <ModSection>
+      <motion.div className="mx-auto my-28 max-w-7xl rounded-xl border-2 border-slate-800 bg-slate-500/5 px-8 pt-20 pb-30 text-white">
+        <div className="">
+          <h1 className="bg-red-400 text-end text-2xl font-bold">
+            Quem usou, aprovou!
+          </h1>
+          <div className="my-16 border-2 border-white py-16">
+            <div className="flex justify-around">
+              <div className="grid grid-cols-3 gap-x-10 border-2 border-red-500">
+                {testimonials.map((item, index) => {
+                  return (
+                    <TestimonialCard
+                      key={index}
+                      clientStatus={item.clientStatus}
+                      description={item.description}
+                      testimonialRating={item.rating}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </ModSection>
   );
 };
 
