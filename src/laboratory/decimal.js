8192c64fd482dabe9db32baf1f7e95f669f227eb
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-02 12:44:50 -0300

    chore(laboratory): introduce my new concept: Laboratory! Scripts for logic tests or whatever test  will be add in this folder

---
+++ src/laboratory/decimal.js
@@ -0,0 +1,11 @@
+const valor = 0.3;
+
+let decimal = valor % 1;
+
+// decimal = parseFloat(decimal.toFixed(2));
+
+// console.log(`A parte decimal é: ${decimal}`);
+
+console.log(Math.ceil(valor))
+
+
