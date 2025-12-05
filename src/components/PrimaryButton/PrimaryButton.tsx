a83d8773bf8aff2c507ae9347ae5dad2e591b555
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-05 12:52:56 -0300

    chore(plans): change this Button name to Primary Button, like a variant

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,17 +1,12 @@
 import { ArrowRight } from "lucide-react";
 import { defaultTransition } from "../../utils/defaultVariables";
+import type { IPrimaryButtonProps } from "../types/components.types";
 
-interface ButtonProps {
-  label: string;
-  slug: string;
-  width: number;
-}
-
-const PrimaryButton = ({ label, slug, width }: ButtonProps) => {
+const PrimaryButton = ({ label, slug, width }: IPrimaryButtonProps) => {
   return (
     <a
       href={slug}
-      className={`flex w-fit flex-row items-center gap-1.5 rounded-xl bg-fuchsia-500 px-5 py-2 font-semibold text-white ${defaultTransition} hover:bg-fuchsia-600`}
+      className={`flex w-fit flex-row items-center gap-1.5 rounded-xl bg-fuchsia-500 px-5 py-2 font-semibold text-white select-none ${defaultTransition} hover:bg-fuchsia-600`}
     >
       {label}
       <ArrowRight width={width} className="flex items-center justify-center" />
