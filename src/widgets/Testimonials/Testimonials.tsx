d36ee5fe54d5b914042615b4639387f1296d1563
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-05 14:26:17 -0300

    feat(Resources): Finished Testimonials section

--- src/widgets/Testimonials/Testimonials.tsx
+++ src/widgets/Testimonials/Testimonials.tsx
@@ -5,19 +5,29 @@ import { testimonials } from "../../assets/testimonials";
 
 const Testimonials = () => {
   return (
-    <ModSection>
-      <motion.div className="mx-auto my-28 max-w-7xl rounded-xl border-2 border-slate-800 bg-slate-500/5 px-8 pt-20 pb-30 text-white">
+    <ModSection id="depoimentos">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.75 }}
+        viewport={{ once: true }}
+        className="mx-auto my-30 max-w-7xl rounded-3xl border-2 border-slate-800 bg-slate-500/5 px-8 pt-20 pb-30 text-white"
+      >
         <div className="">
-          <h1 className="bg-red-400 text-end text-2xl font-bold">
+          <h1 className="text-center text-3xl font-bold select-none">
             Quem usou, aprovou!
           </h1>
-          <div className="my-16 border-2 border-white py-16">
+          <p className="mt-1 -mb-2 text-center font-semibold text-gray-400 select-none">
+            Algumas de Nossas Avaliações...
+          </p>
+          <div className="my-16">
             <div className="flex justify-around">
-              <div className="grid grid-cols-3 gap-x-10 border-2 border-red-500">
+              <div className="grid grid-cols-3 gap-x-10">
                 {testimonials.map((item, index) => {
                   return (
                     <TestimonialCard
                       key={index}
+                      clientName={item.clientName}
                       clientStatus={item.clientStatus}
                       description={item.description}
                       testimonialRating={item.rating}
