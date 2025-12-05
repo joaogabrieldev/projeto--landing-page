d212e6249f6472322b5356b31a19272d59b02e8d
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-05 13:17:38 -0300

    feat: created piece of component 'Badge'

---
+++ src/pieces/Badge/Badge.tsx
@@ -0,0 +1,15 @@
+interface IPriceBadgeProps {
+  label: string;
+}
+
+const Badge = ({ label }: IPriceBadgeProps) => {
+  return (
+    <div className="flex items-center justify-center rounded-2xl bg-fuchsia-600">
+      <span className="px-3 py-1 text-xs font-bold text-white select-none">
+        {label.toUpperCase()}
+      </span>
+    </div>
+  );
+};
+
+export default Badge;
