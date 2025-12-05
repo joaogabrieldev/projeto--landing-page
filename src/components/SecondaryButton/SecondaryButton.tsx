91a0f957381d5d1ef8ad5d5ba5bd074c966a0ab0
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-05 12:56:40 -0300

    feat: created second variant button

--- src/components/SecondaryButton/SecondaryButton.tsx
+++ src/components/SecondaryButton/SecondaryButton.tsx
@@ -1,15 +1,11 @@
 import { defaultTransition } from "../../utils/defaultVariables";
+import type { ISecondButtonProps } from "../types/components.types";
 
-interface ButtonProps {
-  label: string;
-  slug: string;
-}
-
-const SecondaryButton = ({ label, slug }: ButtonProps) => {
+const SecondaryButton = ({ label, slug }: ISecondButtonProps) => {
   return (
     <a
       href={slug}
-      className={`flex w-fit flex-row items-center gap-1.5 rounded-xl border-2 border-gray-700 px-6 py-3 font-medium text-white ${defaultTransition} hover:border-gray-500`}
+      className={`flex w-fit flex-row items-center gap-1.5 rounded-xl border-2 border-gray-700 px-6 py-3 font-medium text-white select-none ${defaultTransition} hover:border-gray-500`}
     >
       {label}
     </a>
