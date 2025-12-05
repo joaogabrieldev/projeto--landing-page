f7835fc9afac2dcca65957de00bd077338acf9eb
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-05 12:43:21 -0300

    chore(assets): created plans data file

---
+++ src/assets/plans.tsx
@@ -0,0 +1,95 @@
+import type { Plans } from "./types/assets.types";
+
+export const plans: Plans[] = [
+  {
+    title: "Pacote One Page",
+    price: 150,
+    originalPrice: 250,
+    emphasis: false,
+    description:
+      "Modernidade e objetividade para sua presença digital. Concentre sua história e serviços em uma única página interativa, oferecendo uma experiência de navegação fluida, veloz e focada em retenção, sem recarregamentos.",
+    recommendation: "Landing Pages, Portfólios, Links na Bio ou Lançamentos.",
+    beneficts: [
+      {
+        title: "Estrutura de Página Única (SPA) Moderna e Responsiva",
+      },
+      {
+        title: "CTAs Avançadas",
+      },
+      {
+        title: "Carregamento instantâneo",
+      },
+      {
+        title: "Integração com Redes Sociais",
+      },
+      {
+        title: "Otimização para SEO",
+      },
+    ],
+  },
+  {
+    title: "Pacote Personalizado",
+    price: 750,
+    originalPrice: 900,
+    emphasis: true,
+    description:
+      "A escolha certa para profissionalizar sua marca, aliando design e alta performance. Um site desenvolvido com código limpo, que carrega rápido em dispositivos móveis e já vem com SEO técnico configurado para destacar sua empresa nos buscadores e atrair mais clientes.",
+    recommendation: "E-commerces e Grandes Players do Mercado",
+    beneficts: [
+      {
+        title: "Design Responsivo e Totalmente Exclusivo",
+      },
+      {
+        title: "Até 10 páginas",
+      },
+      {
+        title: "1 ano de Domínio Incluso",
+      },
+      {
+        title: "SEO Avançado",
+      },
+      {
+        title: "CTAs Avançadas",
+      },
+      {
+        title: "Formulário de contato",
+      },
+      {
+        title: "Integração com Redes Sociais e Pixel (Meta)",
+      },
+      {
+        title: "Suporte Prioritário",
+      },
+    ],
+  },
+  {
+    title: "Pacote Standard",
+    price: 400,
+    originalPrice: 600,
+    emphasis: false,
+    description:
+      "A escolha certa para profissionalizar sua marca, aliando design e alta performance. Um site desenvolvido com código limpo, que carrega rápido em dispositivos móveis e já vem com SEO técnico configurado para destacar sua empresa nos buscadores e atrair mais clientes.",
+    recommendation:
+      "Pequenas empresas, Consultórios, Advogados e Prestadores de Serviço.",
+    beneficts: [
+      {
+        title: "Website Moderno e Responsivo",
+      },
+      {
+        title: "CTAs Avançadas",
+      },
+      {
+        title: "Até 5 páginas",
+      },
+      {
+        title: "Formulário de contato",
+      },
+      {
+        title: "Integração com Redes Sociais",
+      },
+      {
+        title: "Otimização para SEO",
+      },
+    ],
+  },
+];
