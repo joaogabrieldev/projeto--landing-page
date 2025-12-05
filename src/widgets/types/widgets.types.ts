6c92b137781fea92ec4a8d4a558fd5d29248560f
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-05 14:27:08 -0300

    chore(widgets): create this files to keep all types and interface of this module

---
+++ src/widgets/types/widgets.types.ts
@@ -0,0 +1,13 @@
+import type React from "react";
+
+//* BodyPinkGlow
+export interface IBodyGlowProps {
+  children: React.ReactNode;
+}
+
+//* ModSection
+
+export interface IModSectionProps {
+  children: React.ReactNode;
+  id: string;
+}
