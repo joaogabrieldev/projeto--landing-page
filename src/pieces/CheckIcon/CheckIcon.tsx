5eaf4f3613523470a9809f824ced871ddd2bbc18
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-12-05 13:30:46 -0300

    chore: separate Props interface

--- src/pieces/CheckIcon/CheckIcon.tsx
+++ src/pieces/CheckIcon/CheckIcon.tsx
@@ -1,8 +1,5 @@
 import { Check } from "lucide-react";
-
-interface ICheckProps {
-  iconWidth: number;
-}
+import type { ICheckProps } from "../types/pieces.types";
 
 const CheckIcon = ({ iconWidth }: ICheckProps) => {
   return <Check className="text-fuchsia-500" width={iconWidth} />;
